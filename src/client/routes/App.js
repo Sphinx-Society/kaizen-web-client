3e32cd55e43a13cc58287322212794f343685e30
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2020-05-04 04:31:37 -0500

    SSKW-1 Added content to same files to render a basic error 404 as default

---
+++ src/client/routes/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import NotFound from '../pages/NotFound'
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+)
+
+export default App
