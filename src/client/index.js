62aa6c6ab88da5a11719e5210545ef2ea47d9bb3
Omar Flores Grimontt <dev@omarefg.com>
2020-05-05 21:04:32 -0500

    SSKW-14 Added HMR to avoid refreshing the browser when developing.

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './routes/App'
+import React from 'react';
+import { render } from 'react-dom';
+import App from './routes/App';
+import './styles/index.scss';
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const container = document.getElementById('app');
+
+function renderApp() {
+  render(<App />, container);
+}
+
+renderApp();
+
+if (module.hot) {
+  module.hot.accept('./routes/App', () => {
+    renderApp();
+  });
+}
