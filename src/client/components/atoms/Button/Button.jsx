7022870ab513a5a0b8287c04016d4713b403b9bd
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2020-05-11 07:00:15 -0500

    SSKW-20 Finished Button to use as text / icon or both

--- src/client/components/atoms/Button/Button.jsx
+++ src/client/components/atoms/Button/Button.jsx
@@ -10,7 +10,10 @@ const Button = (props) => {
     onClick,
     children,
     type,
+    icon,
     iconMode,
+    iconPosition,
+    iconCenter,
     color,
     className,
     disabled,
@@ -53,15 +56,32 @@ const Button = (props) => {
     [className]: className,
   });
 
+  const btnContainerStyles = clsx({
+    'btn__container': true,
+    'btn__container--cols2': !isIcon && (icon && iconCenter),
+    'btn__container--cols3': !isIcon && (icon && !iconCenter && !iconPosition),
+    'btn__container--cols3-right': !isIcon && (icon && !iconCenter && iconPosition),
+  });
+
   return (
     <button
-      type={typeButtons[type]}
       onClick={onClick}
+      type={typeButtons[type]}
       className={btnStyles}
       disabled={disabled}
       form={form}
     >
-      {children}
+      { icon ? (
+        <div className={btnContainerStyles}>
+          {!iconPosition
+            && <div className='btn__container--icon'>{icon}</div>}
+          <div className='btn__container--children'>{children}</div>
+          {iconPosition
+            && <div className='btn__container--icon'>{icon}</div>}
+        </div>
+      ) : (
+        children
+      )}
     </button>
   );
 };
@@ -71,13 +91,22 @@ Button.propTypes = {
   onClick: PropTypes.func.isRequired,
 
   /** The text inside the button or another component */
-  children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
 
   /** Specify the type of the button */
   type: PropTypes.string,
 
-  /* Spacing modes for icon type */
+  /** Element Icon type */
+  icon: PropTypes.oneOfType([PropTypes.func, PropTypes.node]),
+
+  /** Spacing modes for icon type */
   iconMode: PropTypes.string,
+
+  /** Indicates on which side of the text the icon is placed. left = false | Right = true */
+  iconPosition: PropTypes.bool,
+
+  /** Icon and other elements will be centered. */
+  iconCenter: PropTypes.bool,
 
   /** Boolean class to specify if the color button is primary, secondary or warning */
   color: PropTypes.string,
@@ -94,7 +123,10 @@ Button.propTypes = {
 
 Button.defaultProps = {
   type: 'button',
+  icon: undefined,
   iconMode: undefined,
+  iconPosition: false,
+  iconCenter: false,
   color: 'default',
   className: undefined,
   disabled: false,
