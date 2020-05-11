e023eafbb992219677ac384485840c2323545bbc
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2020-05-11 03:37:56 -0500

    SSKW-20 Added feature at Button to use as icon Button

--- src/client/components/atoms/Button/Button.jsx
+++ src/client/components/atoms/Button/Button.jsx
@@ -8,6 +8,7 @@ const Button = (props) => {
   const {
     children,
     type,
+    iconMode,
     onClick,
     className,
     disabled,
@@ -22,25 +23,32 @@ const Button = (props) => {
     icon: 'button',
   };
 
-  function colorExists(key, value) {
-    const colors = ['primary', 'secondary', 'warning', 'light'];
-    return colors[key] === value;
-  }
-  function typeIsIcon() {
-    return type === 'icon';
+  const colors = ['primary', 'secondary', 'warning', 'light'];
+  const iconModes = ['circle-0', 'circle', '1', '2', '3', '4'];
+
+  const isIcon = type === 'icon';
+
+  function applyStyleBool(isIcon, ary, valuetest, index) {
+    return isIcon && (ary[index] === valuetest);
   }
 
   const btnStyles = clsx({
-    'ssk--spacing': !typeIsIcon(),
-    'btn': !typeIsIcon(),
-    'btn--primary': colorExists(0, color) && !typeIsIcon(),
-    'btn--secondary': colorExists(1, color) && !typeIsIcon(),
-    'btn--warning': colorExists(2, color) && !typeIsIcon(),
-    'btn-icon': typeIsIcon(),
-    'btn-icon--primary': colorExists(0, color) && typeIsIcon(),
-    'btn-icon--secondary': colorExists(1, color) && typeIsIcon(),
-    'btn-icon--warning': colorExists(2, color) && typeIsIcon(),
-    'btn-icon--light': colorExists(3, color) && typeIsIcon(),
+    'ssk--spacing': !isIcon,
+    'btn': !isIcon,
+    'btn--primary': applyStyleBool(!isIcon, colors, color, 0),
+    'btn--secondary': applyStyleBool(!isIcon, colors, color, 1),
+    'btn--warning': applyStyleBool(!isIcon, colors, color, 2),
+    'btn-icon': isIcon,
+    'btn-icon--primary': applyStyleBool(isIcon, colors, color, 0),
+    'btn-icon--secondary': applyStyleBool(isIcon, colors, color, 1),
+    'btn-icon--warning': applyStyleBool(isIcon, colors, color, 2),
+    'btn-icon--light': applyStyleBool(isIcon, colors, color, 3),
+    'btn-icon--space-mode-circle-0': applyStyleBool(isIcon, iconModes, iconMode, 0),
+    'btn-icon--space-mode-circle': applyStyleBool(isIcon, iconModes, iconMode, 1),
+    'btn-icon--space-mode-1': applyStyleBool(isIcon, iconModes, iconMode, 2),
+    'btn-icon--space-mode-2': applyStyleBool(isIcon, iconModes, iconMode, 3),
+    'btn-icon--space-mode-3': applyStyleBool(isIcon, iconModes, iconMode, 4),
+    'btn-icon--space-mode-4': applyStyleBool(isIcon, iconModes, iconMode, 5),
     [className]: className,
   });
 
@@ -60,6 +68,9 @@ const Button = (props) => {
 Button.propTypes = {
   /** Specify the type of the button */
   type: PropTypes.string,
+
+  /* Spacing modes for icon type */
+  iconMode: PropTypes.string,
 
   /** Function that will be called on click event. */
   onClick: PropTypes.func.isRequired,
@@ -82,6 +93,7 @@ Button.propTypes = {
 
 Button.defaultProps = {
   type: 'button',
+  iconMode: undefined,
   className: undefined,
   disabled: false,
   form: undefined,
