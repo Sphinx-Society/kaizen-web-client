d774e28ca89ede984f02c9d7dc7ef11e9a6aaa01
Omar Flores Grimontt <dev@omarefg.com>
2020-05-12 20:20:39 -0500

    SSKW-4 Added server that will serve the app on heroku using express.

--- server.js
+++ server.js
@@ -1,62 +1,17 @@
-const { createServer } = require('http');
-const { extname } = require('path');
-const { readFile } = require('fs');
+const express = require('express');
+const { join } = require('path');
 
-const port = 8080;
-const urlPrefix = './dist';
+const app = express();
 
-const mimeTypes = {
-  '.html': 'text/html',
-  '.js': 'text/javascript',
-  '.css': 'text/css',
-  '.png': 'image/png',
-  '.jpg': 'image/jpg',
-  '.svg': 'image/svg+xml',
-  '.woff': 'application/font-woff',
-  '.ttf': 'application/font-ttf',
-  '.eot': 'application/vnd.ms-fontobject',
-  '.json': 'application/json',
-  '.gif': 'image/gif',
-  '.wav': 'audio/wav',
-  '.mp4': 'video/mp4',
-  '.otf': 'application/font-otf',
-  '.wasm': 'application/wasm',
-};
+app.use(express.static(join(__dirname, 'dist')));
 
-function printErrorAndExit(error) {
-  console.log(error);
-  process.exit(1);
-}
-
-function getFile(req) {
-  let file = `${urlPrefix}${req.url}`;
-  if (file === './dist/') {
-    file = './dist/index.html';
-  }
-  return file;
-}
-
-const server = createServer((req, res) => {
-  const file = getFile(req);
-  const ext = extname(file);
-  console.log(ext);
-  const contentType = mimeTypes[ext] || 'application/octet-stream';
-
-  readFile(file, (error, content) => {
-    if (error) {
-      res.writeHead(500);
-      res.end();
-    } else {
-      res.writeHead(200, { 'Content-Type': contentType });
-      res.end(content, 'utf-8');
-    }
-  });
+app.get('*', (req, res) => {
+  res.sendFile(join(`${__dirname}/dist/index.html`));
 });
 
-server.listen(port, (error) => {
-  if (error) {
-    printErrorAndExit(error);
-  }
-  const url = `http://localhost:${port}`;
-  console.log(`Server running at ${url}`);
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Web client is listening on port ${port}`);
 });
+
