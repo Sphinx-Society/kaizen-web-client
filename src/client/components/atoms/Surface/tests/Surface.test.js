5aaf1526ca56d054ddbf90843d52a72cb646856e
Omar Flores Grimontt <dev@omarefg.com>
2020-05-13 21:42:33 -0500

    SSKW-21 Removed unnecesary import from surface test.

--- src/client/components/atoms/Surface/tests/Surface.test.js
+++ src/client/components/atoms/Surface/tests/Surface.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { create } from 'react-test-renderer';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Surface from '../Surface';
