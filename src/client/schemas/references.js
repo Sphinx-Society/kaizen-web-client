5e76227cb44fa55649b5f00465fab67ed59b2ba7
Omar Flores Grimontt <dev@omarefg.com>
2020-05-18 07:15:54 -0500

    SSKW-54 Created schemas references to import their data inside other stories and components.

---
+++ src/client/schemas/references.js
@@ -0,0 +1,9 @@
+export const TABLE_COLUMN_REFERENCE = {
+  path: 'schemas-tablecolumn--page',
+  name: 'TableColumn Schema',
+};
+
+export const TABLE_ROW_REFERENCE = {
+  path: 'schemas-tablerow--page',
+  name: 'TableRow Schema',
+};
