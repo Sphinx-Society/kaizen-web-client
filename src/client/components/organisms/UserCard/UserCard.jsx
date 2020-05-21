0b5c4b56f6b29acd8276205df70236bbd94bb565
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2020-05-21 17:36:22 -0500

    SSKW-73 Modified UserCard molecule to improve usage and display

--- src/client/components/organisms/UserCard/UserCard.jsx
+++ src/client/components/organisms/UserCard/UserCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import {
   FaEye as ViewIcon,
   FaPen as EditIcon,
@@ -11,53 +12,91 @@ import ReadableField from '../../atoms/ReadableField/ReadableField';
 import Button from '../../atoms/Button/Button';
 
 import './UserCard.scss';
-import { prototype } from 'enzyme-adapter-react-16';
 
 const UserCard = (props) => {
-  const { data } = props;
+  const {
+    data,
+    isAdminWhoView,
+    linkToViewMore,
+    linkToEdit,
+    onClickDelete,
+    sizeIcons,
+  } = props;
 
   return (
     <article className='user-card ssk--spacing surface__container'>
       <ListReadableFields className='user-card__info'>
-        {data.map((item) => <ReadableField title={item.title} description={item.description} />)}
+        {data.map((item, index) => (
+          <ReadableField
+            title={item.title}
+            description={item.description}
+            key={index}
+          />
+        ))}
       </ListReadableFields>
 
       <div className='user-card__actions'>
-        <Button
-          icon={<ViewIcon size='3em' />}
-          type='icon'
-          iconMode='2'
-          onChange={() => null}
-          color='primary'
-          className='ssk--boxShadow'
-        />
-        <Button
-          icon={<EditIcon size='3em' />}
-          type='icon'
-          iconMode='2'
-          onChange={() => null}
-          className='ssk--boxShadow'
-        />
-        <Button
-          icon={<DeleteIcon size='3em' />}
-          type='icon'
-          iconMode='2'
-          onChange={() => null}
-          color='warning'
-          className='ssk--boxShadow'
-        />
+        <Link to={linkToViewMore}>
+          <Button
+            icon={<ViewIcon size={sizeIcons} />}
+            type='icon'
+            iconMode='2'
+            onClick={() => null}
+            color='primary'
+            className='ssk--boxShadow'
+          />
+        </Link>
+
+        {isAdminWhoView && (
+          <>
+            <Link to={linkToEdit}>
+              <Button
+                icon={<EditIcon size={sizeIcons} />}
+                type='icon'
+                iconMode='2'
+                onClick={() => null}
+                className='ssk--boxShadow'
+              />
+            </Link>
+
+            <Button
+              icon={<DeleteIcon size={sizeIcons} />}
+              type='icon'
+              iconMode='2'
+              onClick={onClickDelete}
+              color='warning'
+              className='ssk--boxShadow'
+            />
+          </>
+        )}
       </div>
     </article>
   );
 };
 
 UserCard.propTypes = {
-  /** It is the array of objects  */
+  /** It is the array of objects to receive descriptive information */
   data: PropTypes.arrayOf(
     PropTypes.shape({ title: PropTypes.string, description: PropTypes.string }),
   ).isRequired,
+  /** It is the administrator who will see the information  */
+  isAdminWhoView: PropTypes.bool,
+  /** Path to view more user details  */
+  linkToViewMore: PropTypes.string,
+  /** Path to edit user details */
+  linkToEdit: PropTypes.string,
+  /** Action to delete specific source */
+  onClickDelete: PropTypes.func,
+  /** Size of action icons */
+  sizeIcons: PropTypes.string,
 };
 
-UserCard.defaultProps = {};
+UserCard.defaultProps = {
+  isAdminWhoView: false,
+  linkToViewMore: '/',
+  linkToEdit: '/',
+  onClickDelete: undefined,
+  sizeIcons: '3em',
+};
 
 export default UserCard;
