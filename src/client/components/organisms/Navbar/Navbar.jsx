e7bea00135c2b8ed4533bed97f405c688426fb16
Andrés Santana <drew.lizcano@gmail.com>
2020-05-23 12:25:04 -0500

    SSKW-93 Added Navbar changes to responsive view

--- src/client/components/organisms/Navbar/Navbar.jsx
+++ src/client/components/organisms/Navbar/Navbar.jsx
@@ -1,59 +1,61 @@
 import React from 'react';
+import clsx from 'clsx';
 import PropTypes from 'prop-types';
-import { FaUser as UserIcon } from 'react-icons/fa';
-import {
-  MdFindInPage as FindIcon,
-  MdSettings as SettingsIcon,
-} from 'react-icons/md';
-import Button from '../../atoms/Button/Button';
+import { NavLink } from 'react-router-dom';
+import { MdFindInPage as ExamsIcon } from 'react-icons/md';
+import { GoGear as SettingsIcon } from 'react-icons/go';
+import { AiFillHome as HomeIcon } from 'react-icons/ai';
+import Logo from '../../atoms/Logo/Logo';
 
 import './Navbar.scss';
 
 const Navbar = (props) => {
-  const { onClickSearch, onClickProfile, onClickSettings } = props;
-
+  const { isAdmin } = props ;
+  const navbarStyles = clsx({
+    'navbar': true,
+  });
   return (
-    <nav className='navbar'>
-      <div className='navbar__logo'>
-        <svg width='3em' height='3em' viewBox='-10 -10 48 48' fill='none' xmlns='http://www.w3.org/2000/svg'>
-          <path fillRule='evenodd' clipRule='evenodd' d='M28.4556 1.11816H17.4357C13.919 3.95512 11.6934 8.15066 11.6934 12.8318C11.6934 17.2755 13.6989 21.2816 16.9107 24.1034H27.739L13.7908 13.0201L28.4556 1.11816Z' fill='#B5E2FA' />
-          <path fillRule='evenodd' clipRule='evenodd' d='M7.6719 0.676086C3.55449 3.16739 0.810547 7.62998 0.810547 12.7207C0.810547 17.8114 3.55449 22.274 7.6719 24.7653V0.676086Z' fill='#0066FF' />
-        </svg>
+    <nav className={navbarStyles}>
+      <div className='navbar__logo .ssk--boxShadow'>
+        <Logo size='30px' />
       </div>
 
       <div className='navbar__buttons'>
-        <Button
-          icon={<FindIcon size='3em' />}
-          type='icon'
-          color='primary'
-          onClick={onClickSearch}
-        />
-        <Button
-          icon={<UserIcon size='3em' />}
-          type='icon'
-          color='primary'
-          onClick={onClickProfile}
-        />
-        <Button
-          icon={<SettingsIcon size='3em' />}
-          type='icon'
-          color='primary'
-          onClick={onClickSettings}
-        />
+        <div className='button--container'>
+          <NavLink className='link' to='/home' activeClassName='is-selected'>
+            <HomeIcon size='2em' />
+          </NavLink>
+          <dd>Inicio</dd>
+        </div>
+
+        {isAdmin && (
+          <div className='button--container'>
+            <NavLink to='/exams' activeClassName='is-selected'>
+              <ExamsIcon size='2em' />
+            </NavLink>
+            <dd>Examenes</dd>
+          </div>
+        )}
+
+        <div className='button--container'>
+          <NavLink to='/settings' activeClassName='is-selected'>
+            <SettingsIcon size='2em' />
+          </NavLink>
+          <dd>Ajustes</dd>
+        </div>
+
       </div>
     </nav>
   );
 };
-
 Navbar.propTypes = {
-  /** Function that will be called on click event by btn-icon Search */
-  onClickSearch: PropTypes.func.isRequired,
+  /** It is the administrator who will see the information  */
+  isAdmin: PropTypes.bool,
 
-  /** Function that will be called on click event by btn-icon user Profile */
-  onClickProfile: PropTypes.func.isRequired,
+};
 
-  /** Function that will be called on click event by btn-icon Settings / Settings */
-  onClickSettings: PropTypes.func.isRequired,
+Navbar.defaultProps = {
+  isAdmin: false,
 };
 
 export default Navbar;
--- src/client/components/organisms/Navbar/test/Navbar.test.js
+++ src/client/components/organisms/Navbar/test/Navbar.test.js
@@ -1,82 +1,40 @@
 import React from 'react';
 import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { MemoryRouter } from 'react-router-dom';
+import ProviderMock from '../../../../__mocks__/redux/ProviderMock';
+
 import Navbar from '../Navbar';
 
 configure({ adapter: new Adapter() });
+const setUp = () => {
+  const componenttoRender = mount(
 
-describe('Navbar', () => {
-  describe('There are some things by default, in static HTML', () => {
-    const navbar = mount(
-      <Navbar
-        onClickSearch={() => null}
-        onClickProfile={() => null}
-        onClickSettings={() => null}
-      />,
-    );
+    <MemoryRouter>
+      <ProviderMock>
+        <Navbar />
+      </ProviderMock>
+    </MemoryRouter>
+    ,
+  );
+  return componenttoRender;
+};
 
-    describe('Default classes', () => {
-      test('Navbar has a "class" equal to ".navbar":', () => {
-        expect(navbar.find('nav').hasClass('navbar')).toBe(true);
-      });
+describe('Navbar organism', () => {
+  let component;
+  beforeEach(() => {
+    component = setUp();
 
-      test('Container logo has a "class" equal to ".navbar__logo":', () => {
-        expect(navbar.find('nav > div').at(0).hasClass('navbar__logo')).toBe(true);
-      });
-
-      test('Container buttons has a "class" equal to ".navbar__buttons":', () => {
-        expect(navbar.find('nav > div').at(1).hasClass('navbar__buttons')).toBe(true);
-      });
-    });
-
-    describe('Default props in actions buttons', () => {
-      test('have a "type" equal to "icon":', () => {
-        const btn = navbar.find('Button');
-        expect(btn.at(0).prop('type')).toBe('icon');
-        expect(btn.at(1).prop('type')).toBe('icon');
-        expect(btn.at(2).prop('type')).toBe('icon');
-      });
-
-      test('have a prop "icon":', () => {
-        const btn = navbar.find('Button');
-        expect(btn.at(0).prop('icon')).toBeTruthy();
-        expect(btn.at(1).prop('icon')).toBeTruthy();
-        expect(btn.at(2).prop('icon')).toBeTruthy();
-      });
-    });
+  });
+  test('Should render without errors', () => {
+    expect(component.length).toEqual(1);
   });
 
-  describe('Click event activated', () => {
-    function eventOnClickSetup() {
-      const onClickFn = jest.fn();
-      return {
-        onClickFn,
-        'navbar': mount(
-          <Navbar
-            onClickSearch={onClickFn}
-            onClickProfile={onClickFn}
-            onClickSettings={onClickFn}
-          />,
-        ),
-      };
-    }
-
-    test('should execute a function by first button:', () => {
-      const { navbar, onClickFn } = eventOnClickSetup();
-      navbar.find('Button').at(0).simulate('click');
-      expect(onClickFn.mock.calls.length).toEqual(1);
-    });
-
-    test('should execute a function by middle button:', () => {
-      const { navbar, onClickFn } = eventOnClickSetup();
-      navbar.find('Button').at(1).simulate('click');
-      expect(onClickFn.mock.calls.length).toEqual(1);
-    });
-
-    test('should execute a function by last button:', () => {
-      const { navbar, onClickFn } = eventOnClickSetup();
-      navbar.find('Button').at(2).simulate('click');
-      expect(onClickFn.mock.calls.length).toEqual(1);
-    });
+  test('Should render logo', () => {
+    expect(component.find('.navbar__logo').length).toEqual(1);
+  });
+  test('Should render only two icons when user is not admin', () => {
+    const icon = component.find('.button--container');
+    expect(icon.length).toBe(2);
   });
 });
