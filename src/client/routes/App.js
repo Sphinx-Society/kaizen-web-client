4c509e9bccd7655b1c125f758316c016f3ecc1d2
Omar Flores Grimontt <dev@omarefg.com>
2020-05-23 00:46:24 -0500

    SSKW-31 Added AdminExamsCrud page to router to be accessible from browser.

--- src/client/routes/App.js
+++ src/client/routes/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import NotFound from '../components/pages/NotFound';
 import Login from '../components/pages/Login/Login';
+import AdminExamsCrud from '../components/pages/AdminExamsCrud/AdminExamsCrud';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={Login} />
+      <Route exact path='/admin-exams-crud' component={AdminExamsCrud} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
