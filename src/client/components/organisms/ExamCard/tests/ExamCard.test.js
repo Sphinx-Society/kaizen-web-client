a036f663c7c4da824af76c49f3ebeae21427c530
Omar Flores Grimontt <dev@omarefg.com>
2020-05-23 00:45:50 -0500

    SSKW-31 Created ExamCard component to be used by exams crud view in mobile mode

---
+++ src/client/components/organisms/ExamCard/tests/ExamCard.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { configure, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import ExamCard from '../ExamCard';
+
+configure({ adapter: new Adapter() });
+
+describe('ExamCard', () => {
+  describe('Default Classes', () => {
+    const nameMock = 'Blood test';
+    const categoryMock = 'Laboratory';
+    const creationDateMock = 1234568512;
+
+    test('Should have a class ".exam-card"', () => {
+      const examCard = render(
+        <ExamCard
+          name={nameMock}
+          category={categoryMock}
+          creationDate={creationDateMock}
+        />,
+      );
+      expect(examCard.hasClass('exam-card')).toBe(true);
+    });
+
+    test('ExamCard props', () => {
+      const examCard = mount(
+        <ExamCard
+          name={nameMock}
+          category={categoryMock}
+          creationDate={creationDateMock}
+        />,
+      );
+      const {
+        name,
+        category,
+        creationDate,
+      } = examCard.find('ExamCard').props();
+
+      expect(name).toBe(nameMock);
+      expect(category).toBe(categoryMock);
+      expect(creationDate).toBe(creationDateMock);
+    });
+
+    // test('Child "dt" should have a class "readable-field__dd"', () => {
+    //   expect(examCard.find('dd').hasClass('readable-field__dd')).toBeTruthy();
+    // });
+  });
+});
