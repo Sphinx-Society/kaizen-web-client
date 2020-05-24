9534be1c65ec08c62d7815895fdcb3415c9bcf63
Omar Flores Grimontt <dev@omarefg.com>
2020-05-24 11:37:09 -0500

    SSKW-32 Added Exam creator page to router.

--- src/client/routes/App.js
+++ src/client/routes/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NotFound from '../components/pages/NotFound';
 import Login from '../components/pages/Login/Login';
 import ExamsManagement from '../components/pages/ExamsManagement/ExamsManagement';
+import ExamCreator from '../components/pages/ExamCreator/ExamCreator';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={Login} />
       <Route exact path='/exams-management' component={ExamsManagement} />
+      <Route exact path='/exams-management/create' component={ExamCreator} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
