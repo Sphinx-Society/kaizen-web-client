e2a92ef0da18b77e8f16e7c133c895a244c4fcb1
Omar Flores Grimontt <dev@omarefg.com>
2020-05-25 12:29:00 -0500

    SSKW-32 Fixed styles to use responsive design.

--- src/client/components/organisms/Navbar/Navbar.jsx
+++ src/client/components/organisms/Navbar/Navbar.jsx
@@ -6,33 +6,48 @@ import { GoGear as SettingsIcon } from 'react-icons/go';
 import { AiFillHome as HomeIcon } from 'react-icons/ai';
 import Logo from '../../atoms/Logo/Logo';
 
+import { templatesManagement } from '../../../routes/paths';
+
 import './Navbar.scss';
 
 const Navbar = (props) => {
   const { isAdmin } = props ;
+
   return (
     <nav className='navbar'>
-      <div className='navbar__logo .ssk--boxShadow'>
+      <div className='navbar__logo --shadowed'>
         <Logo size='30px' />
       </div>
-      <div className='navbar__buttons'>
-        <div className='button--container'>
-          <NavLink className='link' to='/home' activeClassName='is-selected'>
-            <HomeIcon size='2em' />
+      <div className='navbar__links'>
+        <div className='navbar__links__link'>
+          <NavLink
+            className='navbar__links__link--unselected'
+            activeClassName='navbar__links__link--selected'
+            to='/home'
+          >
+            <HomeIcon />
           </NavLink>
           <dd>Inicio</dd>
         </div>
         {isAdmin && (
-          <div className='button--container'>
-            <NavLink to='/exams' activeClassName='is-selected'>
-              <ExamsIcon size='2em' />
+          <div className='navbar__links__link'>
+            <NavLink
+              className='navbar__links__link--unselected'
+              activeClassName='navbar__links__link--selected'
+              to={templatesManagement()}
+            >
+              <ExamsIcon />
             </NavLink>
             <dd>Examenes</dd>
           </div>
         )}
-        <div className='button--container'>
-          <NavLink to='/settings' activeClassName='is-selected'>
-            <SettingsIcon size='2em' />
+        <div className='navbar__links__link'>
+          <NavLink
+            className='navbar__links__link--unselected'
+            activeClassName='navbar__links__link--selected'
+            to='/settings'
+          >
+            <SettingsIcon />
           </NavLink>
           <dd>Ajustes</dd>
         </div>
@@ -40,12 +55,14 @@ const Navbar = (props) => {
     </nav>
   );
 };
+
 Navbar.propTypes = {
   /** It is the administrator who will see the information  */
   isAdmin: PropTypes.bool,
-
 };
+
 Navbar.defaultProps = {
   isAdmin: false,
 };
+
 export default Navbar;
