69fcbb84d4f924f7e533e3509b47e232c430ac59
Omar Flores Grimontt <dev@omarefg.com>
2020-05-27 07:29:17 -0500

    SSKW-104 Added user actions and reducer to integrate with api.

--- src/client/redux/user/user.actions.requests.js
+++ src/client/redux/user/user.actions.requests.js
@@ -1,5 +1,8 @@
-import * as feedbackActions from '../feedback/feedback.actions';
 import * as userActions from './user.actions';
+import * as feedbackActions from '../feedback/feedback.actions';
+import UserService from '../../services/User';
+
+import { setIsLoading } from '../feedback/feedback.utils';
 
 export const login = (data) => async (dispatch) => {
   dispatch(feedbackActions.setIsLoading({ isLoading: true }));
@@ -12,6 +15,35 @@ export const login = (data) => async (dispatch) => {
       type: 'error',
     }));
   }
+};
+
+export const listUsers = (page = 1, documentId, role) => async (dispatch) => {
+  setIsLoading(dispatch, true);
+  const User = new UserService();
+  try {
+    const {
+      totalPages,
+      currentPage,
+      totalUsers,
+      users,
+    } = await User.listUsers(page, documentId, role);
+    dispatch(userActions.setUsers({
+      users,
+      totalPages,
+      currentPage,
+      totalUsers,
+    }));
+  } catch (error) {
+    dispatch(feedbackActions.setFeedback({
+      feedback: {
+        message: error.message,
+        type: 'error',
+      },
+    }));
+    setIsLoading(dispatch, false);
+    throw error;
+  }
+  setIsLoading(dispatch, false);
 };
 
 export default login;
